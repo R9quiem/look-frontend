f2cfe6102c63fa26c2289f1a9673cb7a0af618f2
efim <efimburachevsky@gmail.com>
2024-04-04 18:40:28 +0300

    Added functionality to switch between ShopСatalogs using ShopCategorySelector

--- src/components/UI/ShopCategorySelector/ShopCategorySelector.jsx
+++ src/components/UI/ShopCategorySelector/ShopCategorySelector.jsx
@@ -1,9 +1,16 @@
+import {NavLink} from "react-router-dom";
 
 
-function ShopCategorySelector(props) {
+function ShopCategorySelector({ selectedCategory, setSelectedCategory }) {
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
   return (
     <nav>
-
+      <button onClick={() => handleCategoryChange('vintage')}>Vintage</button>
+      <button onClick={() => handleCategoryChange('second-hand')}>Second Hand</button>
+      <button onClick={() => handleCategoryChange('our-merch')}>Our Merch</button>
+      <button onClick={() => handleCategoryChange('designer-clothing')}>Designer Clothing</button>
     </nav>
   )
 }
--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
 import Carousel from "../../UI/Carousel/Carousel";
 import ShopCategorySelector from "../../UI/ShopCategorySelector/ShopCategorySelector";
+import {useState} from "react";
+import ShopCatalog from "../../UI/ShowCatalog/ShopCatalog";
 
 const slides = [
   'Slide 1',
@@ -10,10 +12,12 @@ const slides = [
 ];
 
 function HomePage(props){
+  const [selectedCategory, setSelectedCategory] = useState('vintage');
   return (
     <div className="homepage">
       <Carousel slides = {slides}/>
-      <ShopCategorySelector/>
+      <ShopCategorySelector setSelectedCategory={setSelectedCategory} selectedCategory={selectedCategory} />
+      <ShopCatalog selectedCategory={selectedCategory}/>
     </div>
   )
 }
