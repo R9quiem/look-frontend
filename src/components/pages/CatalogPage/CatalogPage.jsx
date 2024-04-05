58f2a31af1baa026c8843a557f7ec002664e8ff4
efim <efimburachevsky@gmail.com>
2024-04-05 15:15:36 +0300

    started working on catalogPage

--- src/components/pages/CatalogPage/CatalogPage.jsx
+++ src/components/pages/CatalogPage/CatalogPage.jsx
@@ -1,9 +1,32 @@
 import "./CatalogPage.sass"
+import {useParams} from "react-router-dom";
+import ProductCard from "../../UI/ProductCard/ProductCard";
+import Product from "../../../models/Product";
 
 function CatalogPage(props){
+  const product = new Product("Nike Sportswear синтетическим наполнителем", 50, 100, 50);
   return (
-    <div>
-
+    <div className="catalog">
+      <nav className="catalog__path">
+        Главная
+      </nav>
+      <div className="catalog__current-category"><h1>Галоши</h1></div>
+      <div className="catalog__main-wrapper">
+        <section className="catalog__category-selector"></section>
+        <div className="catalog__content">
+          <section className="catalog__filters"></section>
+          <div className="catalog__product-list">
+            <ProductCard product={product}/>
+            <ProductCard product={product}/>
+            <ProductCard product={product}/>
+            <ProductCard product={product}/>
+            <ProductCard product={product}/>
+            <ProductCard product={product}/>
+            <ProductCard product={product}/>
+            <ProductCard product={product}/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
