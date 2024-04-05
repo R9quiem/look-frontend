5ab599c039976db478740cdd770840a5fe541dd7
efim <efimburachevsky@gmail.com>
2024-04-05 16:29:49 +0300

    added links to ProductPage in ProductCard

--- src/components/UI/ProductCard/ProductCard.jsx
+++ src/components/UI/ProductCard/ProductCard.jsx
@@ -1,22 +1,25 @@
 import "./ProductCard.sass";
 import ProductPNG from "./product.png"
 import {ReactComponent as DiscountSVG} from "../../../assets/discount-icon.svg"
+import {NavLink} from "react-router-dom";
 function ProductCard({product}){
   return (
-    <div className="product-card">
-      {product.discount&&<DiscountSVG className="product-card__discount-icon"/>}
-      <div className="product-card__image">
-        <img src={ProductPNG} alt="product"/>
-      </div>
-      <section className="product-card__props">
-        <div className="product-card__props__price-wrapper">
-          <span className="product-card__props__price">{product.price}р</span>
-          {product.discount&&<span className="product-card__props__original-price">{product.originalPrice}р</span>}
-          {product.discount&&<span className="product-card__props__discount">{product.discount}%</span>}
+    <NavLink to={`/catalog/${product.key}`}>
+      <div className="product-card">
+        {product.discount&&<DiscountSVG className="product-card__discount-icon"/>}
+        <div className="product-card__image">
+          <img src={ProductPNG} alt="product"/>
         </div>
-        <div className="product-card__props__name">{product.name}</div>
-      </section>
-    </div>
+        <section className="product-card__props">
+          <div className="product-card__props__price-wrapper">
+            <span className="product-card__props__price">{product.price}р</span>
+            {product.discount&&<span className="product-card__props__original-price">{product.originalPrice}р</span>}
+            {product.discount&&<span className="product-card__props__discount">{product.discount}%</span>}
+          </div>
+          <div className="product-card__props__name">{product.name}</div>
+        </section>
+      </div>
+    </NavLink>
   )
 }
 
--- src/components/pages/ProductPage/ProductPage.jsx
+++ src/components/pages/ProductPage/ProductPage.jsx
@@ -1,7 +1,18 @@
+import { useParams } from 'react-router-dom';
+import Product from "../../../models/Product";
+
+const products = [
+  new Product(0,"Nike Sportswear синтетическим наполнителем", 50, 100, 50),
+  new Product(1,"bebebeb", 10011)
+  ]
 
 function ProductPage(props){
+  const { key } = useParams();
+  const product = products.find((item)=>item.key === parseInt(key));
   return (
-    <div></div>
+    <div>
+      {product.name}
+    </div>
   )
 }
 
