df7767cf19673d004f7db80875ef30aa0cd48e48
efim <efimburachevsky@gmail.com>
2024-04-11 13:52:39 +0300

    //

--- src/components/pages/ProductPage/ProductPage.jsx
+++ src/components/pages/ProductPage/ProductPage.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import MultiItemCarousel from "../../UI/MultiItemCarousel/MultiItemCarousel";
 import "./ProductPage.sass";
 import data from "../../../assets/test/data.json";
+import "../../../assets/test/data.json";
 
 const slides = [
   'Slide 1',
@@ -14,6 +15,7 @@ const slides = [
 function ProductPage(props){
   const { key } = useParams();
   const product = data.items.find((item)=>item.id === (key));
+
   return (
     <div className="product-page">
       <section className="product-page__info">
